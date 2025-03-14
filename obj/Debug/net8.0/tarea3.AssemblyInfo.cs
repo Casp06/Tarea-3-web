152e84e41d75f44ec4a9804d2ffd2d088d018f83
Cristopher <cristophersp706@gmail.com>
2025-03-14 19:12:06 -0400

    v1

--- obj/Debug/net8.0/tarea3.AssemblyInfo.cs
+++ obj/Debug/net8.0/tarea3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tarea3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7b11bcd3b91698aaa3dcbabbde26d4cbd53c159")]
 [assembly: System.Reflection.AssemblyProductAttribute("tarea3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tarea3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
